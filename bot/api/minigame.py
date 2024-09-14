d83ff9c7eda5605f75bd129cd6ff1711bf97c287
AKTanara <20730424+AKTanara@users.noreply.github.com>
2024-09-15 01:42:23 +0330

    Update minigame.py
    My version applied

--- bot/api/minigame.py
+++ bot/api/minigame.py
@@ -1,5 +1,4 @@
-
-from typing import Any, Union, Dict, List, Optional, Tuple
+from typing import Any
 
 import aiohttp
 
@@ -8,11 +7,14 @@ from bot.api.http import make_request
 
 async def start_daily_mini_game(
         http_client: aiohttp.ClientSession, mini_game_id: str
-) -> Union[Dict[Any, Any], Any]:
+) -> dict[Any, Any] | Any:
+    url = 'https://api.hamsterkombatgame.io/clicker/start-keys-minigame'
+    response = await http_client.request(method='OPTIONS', url=url, json={}, ssl=False)
+
     response_json = await make_request(
         http_client,
         'POST',
-        'https://api.hamsterkombatgame.io/clicker/start-keys-minigame',
+        url,
         {'miniGameId': mini_game_id},
         'Start Mini Game',
         ignore_status=422
@@ -23,11 +25,14 @@ async def start_daily_mini_game(
 
 async def claim_daily_mini_game(
         http_client: aiohttp.ClientSession, cipher: str, mini_game_id: str
-) -> Tuple[Dict[Any, Any], Dict[Any, Any], int]:
+) -> tuple[dict[Any, Any], dict[Any, Any], int]:
+    url = 'https://api.hamsterkombatgame.io/clicker/claim-daily-keys-minigame'
+    response = await http_client.request(method='OPTIONS', url=url, json={}, ssl=False)
+
     response_json = await make_request(
         http_client,
         'POST',
-        'https://api.hamsterkombatgame.io/clicker/claim-daily-keys-minigame',
+        url,
         {'cipher': cipher, 'miniGameId': mini_game_id},
         'Claim Mini Game',
         ignore_status=422
