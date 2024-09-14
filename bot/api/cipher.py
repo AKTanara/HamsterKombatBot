49315b97812ecbc260abc8cf5b3cd6ac8b9be870
AKTanara <20730424+AKTanara@users.noreply.github.com>
2024-09-15 01:40:11 +0330

    Update cipher.py
    My version applied

--- bot/api/cipher.py
+++ bot/api/cipher.py
@@ -6,10 +6,13 @@ from bot.api.http import make_request
 async def claim_daily_cipher(
         http_client: aiohttp.ClientSession, cipher: str
 ) -> bool:
+    url = 'https://api.hamsterkombatgame.io/clicker/claim-daily-cipher'
+    response = await http_client.request(method='OPTIONS', url=url, json={}, ssl=False)
+
     response_json = await make_request(
         http_client,
         'POST',
-        'https://api.hamsterkombatgame.io/clicker/claim-daily-cipher',
+        url,
         {'cipher': cipher},
         'Claim Daily Cipher',
     )
