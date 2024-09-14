833e1f809543b8b05b3338d1b51e7b2b98962d6d
AKTanara <20730424+AKTanara@users.noreply.github.com>
2024-09-15 01:40:51 +0330

    Update combo.py
    My version applied

--- bot/api/combo.py
+++ bot/api/combo.py
@@ -16,10 +16,13 @@ async def get_combo_cards(http_client: aiohttp.ClientSession) -> dict:
 async def claim_daily_combo(
         http_client: aiohttp.ClientSession
 ) -> bool:
+    url = 'https://api.hamsterkombatgame.io/clicker/claim-daily-combo'
+    response = await http_client.request(method='OPTIONS', url=url, json={}, ssl=False)
+
     response_json = await make_request(
         http_client,
         'POST',
-        'https://api.hamsterkombatgame.io/clicker/claim-daily-combo',
+        url,
         {},
         'Claim Daily Combo',
     )
